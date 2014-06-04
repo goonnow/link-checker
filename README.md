af92e891c31455cb0aaf56f52ed0ac9d999e2cf0
goonnow <goonnow@rocketmail.com>
2014-06-04 12:15:59 +0700

    Polish code and add README also

---
+++ README.md
@@ -0,0 +1,7 @@
+Link Checker
+===
+Find bad links on a page
+
+```
+perl checker.pl http://www.example.com
+```
--- checker.pl
+++ checker.pl
@@ -1,19 +1,24 @@
 use URI;
 use Web::Scraper;
+use AnyEvent;
 use AnyEvent::HTTP;
 use HTTP::Tiny;
 use Timer::Simple ();
 use Data::Dump qw(dump);
+use String::Util ':all';
 
 my $t = Timer::Simple->new();
 $t->start();
 
 my $url =  $ARGV[0] || "http://www.startsiden.no/";
+
 my $links = scraper {
     process "a", "links[]" => sub {
         my $elem = shift;
 
         my $href = $elem->attr('href');
+        $href = trim($href);
+        $href = fullchomp($href);
         $href =~ s|^/|$url|g;
         return {
             title => $elem->attr('title'),
@@ -31,27 +36,42 @@ my @links = remove_dup($res->{links});
 my $total = scalar @links;
 my $bad   = 0;
 
-my $start_time = localtime;
+my $start_time = time;
+my $filename = "check-$start_time.log";
+print "There're $total links in $url\n";
 
-print "There're $total links\n";
-
+my $cv = AnyEvent->condvar;
 for my $l ( @links ) {
-    my $response = HTTP::Tiny->new->get($l->{href});
-    if( $response->{success} ) {
-        print "GOOD\t $l->{href}\n"; 
-    }else{
-        $bad++;
-        print "BAD\t$l\n";
-        print "$response->{status} \t $response->{reason}\n";
-        print dump $l
-    }
+
+    $cv->begin;
+    http_request GET => $l->{href}, sub {
+        my ($body, $hdr) = @_;
+
+        if( $hdr->{Status} =~ /^2/ ){
+            print "GOOD\t $l->{href}\n";
+        }else {
+            print "BAD\t$l->{href} \t $hdr->{Status}\n";
+            $bad++;
+
+            open ( MYFILE, ">>$filename");
+            print MYFILE "$l->{href}\n";
+            close MYFILE;
+
+        }
+        $cv->end;
+
+    };
+
 }
 
+$cv->recv;
 $t->stop();
 
-#print "=== Found $bad bad links from $total links in $url\n";
-#printf "In total took : ", $t->hms;
+print "=== Found $bad bad links from $total links in $url\n";
+printf "In total took : ", $t->hms;
+printf "See also: $filename";
 
+# TODO: Use List instead
 sub remove_dup {
     my $links  = shift;
     my @new;
